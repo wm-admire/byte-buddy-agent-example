5434e24e689ae9d50e085215be022c1562ac97d9
yaohwu <yaohwu@163.com>
2020-05-28 11:15:26 +0800

    clean code except demo code

--- agent/src/main/java/com/dragon/study/bytebuddy/MyAgent.java
+++ agent/src/main/java/com/dragon/study/bytebuddy/MyAgent.java
@@ -1,69 +1,34 @@
 package com.dragon.study.bytebuddy;
 
-import com.dragon.study.bytebuddy.advice.AdviceProfiled;
 import com.dragon.study.bytebuddy.advice.AdviceProfiledTransformer;
-import com.dragon.study.bytebuddy.annotation.EnableMetrics;
-import com.dragon.study.bytebuddy.metrics.MetricsAdviceTransformer;
-import com.dragon.study.bytebuddy.metrics.MetricsTransformer;
-import com.dragon.study.bytebuddy.mysql.MysqlTransformer;
-import com.dragon.study.bytebuddy.okhttp.OkHttpTransformer;
-import com.dragon.study.bytebuddy.redis.RedisAdviceTransformer;
-import com.dragon.study.bytebuddy.redis.RedisTransformer;
-import com.dragon.study.bytebuddy.jersey.JerseyDispatcherTransformer;
-import com.dragon.study.bytebuddy.thrift.ThriftServerTransformer;
-
 import net.bytebuddy.agent.builder.AgentBuilder;
 
 import java.lang.instrument.Instrumentation;
 
-import static net.bytebuddy.matcher.ElementMatchers.isAnnotatedWith;
-import static net.bytebuddy.matcher.ElementMatchers.isInterface;
-import static net.bytebuddy.matcher.ElementMatchers.isStatic;
-import static net.bytebuddy.matcher.ElementMatchers.nameStartsWith;
 import static net.bytebuddy.matcher.ElementMatchers.named;
-import static net.bytebuddy.matcher.ElementMatchers.not;
 
 /**
  * Created by dragon on 16/3/29.
  */
 public class MyAgent {
 
-  public static void premain(String arg, Instrumentation instrumentation) {
+    public static void premain(String arg, Instrumentation instrumentation) {
 
-    String okHttpInterceptor = "com.dragon.study.bytebuddy.okhttp.OkHttpInterceptor";
-    String redisInterceptor = "com.dragon.study.bytebuddy.redis.RedisInterceptor";
-    String metricsInterceptor = "com.dragon.study.bytebuddy.metrics.MetricsInterceptor";
-    String mysqlInterceptor = "com.dragon.study.bytebuddy.mysql.MysqlInterceptor";
-    String thriftServerInterceptor = "com.dragon.study.bytebuddy.thrift.ThriftServerInterceptor";
-    String jerseyInterceptor = "com.dragon.study.bytebuddy.jersey.JerseyDispatcherInterceptor";
+        String okHttpInterceptor = "com.dragon.study.bytebuddy.okhttp.OkHttpInterceptor";
+        String redisInterceptor = "com.dragon.study.bytebuddy.redis.RedisInterceptor";
+        String metricsInterceptor = "com.dragon.study.bytebuddy.metrics.MetricsInterceptor";
+        String mysqlInterceptor = "com.dragon.study.bytebuddy.mysql.MysqlInterceptor";
+        String thriftServerInterceptor = "com.dragon.study.bytebuddy.thrift.ThriftServerInterceptor";
+        String jerseyInterceptor = "com.dragon.study.bytebuddy.jersey.JerseyDispatcherInterceptor";
 
 
-    new AgentBuilder.Default()
-            .with(DebugListener.getListener())
+        new AgentBuilder.Default()
+                .with(DebugListener.getListener())
+                .type(named("com.dragon.study.bytebuddy.advice.AdviceProfiled"))
+                .transform(new AdviceProfiledTransformer())
+                .installOn(instrumentation);
 
-//            .type(named("okhttp3.internal.http.HttpEngine$NetworkInterceptorChain"))
-//            .transform(new OkHttpTransformer(okHttpInterceptor))
-
-              //TODO: Use builder.method(....)
-//            .type(nameStartsWith("redis.clients.jedis").and(not(isInterface())).and(not(isStatic())))
-//            .transform(new RedisTransformer(redisInterceptor))
-        // TODO: Use builder.visit(....)
-//        .type(nameStartsWith("redis.clients.jedis").and(not(isInterface())).and(not(isStatic())))
-//        .transform(new RedisAdviceTransformer())
-            .type(isAnnotatedWith(EnableMetrics.class))
-            .transform(new MetricsAdviceTransformer())
-//            .type(named("com.mysql.jdbc.MysqlIO"))
-//            .transform(new MysqlTransformer(mysqlInterceptor))
-//            .type(named("org.apache.thrift.TBaseProcessor").or(named("org.apache.thrift.TBaseAsyncProcessor")))
-//            .transform(new ThriftServerTransformer(thriftServerInterceptor))
-//            .type(named("org.glassfish.jersey.server.model.internal.AbstractJavaResourceMethodDispatcher"))
-//            .transform(new JerseyDispatcherTransformer(jerseyInterceptor))
-
-//            .type(named("com.dragon.study.bytebuddy.advice.AdviceProfiled"))
-//            .transform(new AdviceProfiledTransformer())
-            .installOn(instrumentation);
-
-  }
+    }
 
 
 }
--- app/src/main/java/com/dragon/study/bytebuddy/timer/TimerPerson.java
+++ app/src/main/java/com/dragon/study/bytebuddy/timer/TimerPerson.java
@@ -3,21 +3,14 @@ package com.dragon.study.bytebuddy.timer;
 import com.dragon.study.bytebuddy.advice.AdviceProfiled;
 import com.dragon.study.bytebuddy.annotation.EnableMetrics;
 import com.dragon.study.bytebuddy.bean.Person;
-
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
-import redis.clients.jedis.Jedis;
 
 /**
  * Created by dragon on 16/3/28.
@@ -26,86 +19,54 @@ import redis.clients.jedis.Jedis;
 @EnableMetrics
 public class TimerPerson {
 
-  @Autowired
-  private Person person;
+    @Autowired
+    private Person person;
 
-  //  @Scheduled(fixedDelay = 5000L, initialDelay = 1000L)
-  public void httpClientTest() {
-    OkHttpClient client = new OkHttpClient();
-    String response;
-    try {
-      response = getResponse(client, "http://enjoy.ricebook.com/");
-    } catch (IOException e) {
-      e.printStackTrace();
-      response = "request error";
+    //  @Scheduled(fixedDelay = 5000L, initialDelay = 1000L)
+    public void httpClientTest() {
+        OkHttpClient client = new OkHttpClient();
+        String response;
+        try {
+            response = getResponse(client, "http://enjoy.ricebook.com/");
+        } catch (IOException e) {
+            e.printStackTrace();
+            response = "request error";
+        }
+
+        System.out.println(response);
     }
 
-    System.out.println(response);
-  }
+    private String getResponse(OkHttpClient client, String url) throws IOException {
+        Request request = new Request.Builder().url("http://enjoy.ricebook.com/").build();
 
-  private String getResponse(OkHttpClient client, String url) throws IOException {
-    Request request = new Request.Builder().url("http://enjoy.ricebook.com/").build();
+        Response response = client.newCall(request).execute();
+        return String.valueOf(response.code());
+    }
 
-    Response response = client.newCall(request).execute();
-    return String.valueOf(response.code());
-  }
-
-  //  @Scheduled(fixedDelay = 10000L, initialDelay = 3000L)
-  public void redisTest() {
-    System.out.println(person.toString() + " calling redis, time is " + System.currentTimeMillis());
-    Jedis jedis = new Jedis("127.0.0.1", 6379);
-    jedis.set("a", "b");
-    String value = jedis.get("a");
-    System.out.println("key is a, value is " + value);
-    jedis.info();
-    jedis.close();
-  }
-
-//  @Count(name = "test.count")
+    //  @Count(name = "test.count")
 //  @Scheduled(fixedDelay = 3000L, initialDelay = 1000L)
-  public void testCount() {
-    System.out.println("begin test count");
-    try {
-      Thread.sleep(500);
-    } catch (InterruptedException e) {
-      e.printStackTrace();
+    public void testCount() {
+        System.out.println("begin test count");
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("end test count");
     }
-    System.out.println("end test count");
-  }
 
-  //  @Scheduled(fixedDelay = 7000L, initialDelay = 7000L)
-  public void testMysql() {
-    System.out.println("begin test mysql jdbc");
-    try {
-      Class.forName("com.mysql.jdbc.Driver").newInstance();
-      Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/demo", "root", "");
-      PreparedStatement pp = conn.prepareStatement("select * from demo_table where age = ?");
-      pp.setInt(1, 30);
-      ResultSet re = pp.executeQuery();
-      String result;
-      if (re.next()) {
-        result = "name is :" + re.getString(1) + ", age is :" + re.getInt(2) + ", score is :" + re
-            .getInt(3);
-        System.out.println("result is " + result);
-      }
-      conn.close();
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
-    System.out.println("end test mysql jdbc");
-  }
 
     @Scheduled(fixedDelay = 3000L, initialDelay = 3000L)
-  public void testAdvice() {
-    System.out.println("begin test advice");
-    AdviceProfiled adviceProfiled = new AdviceProfiled();
-    adviceProfiled.profile(1000, "test");
-    System.out.println("end test advice");
-  }
+    public void testAdvice() {
+        System.out.println("begin test advice");
+        AdviceProfiled adviceProfiled = new AdviceProfiled();
+        adviceProfiled.profile(1000, "test");
+        System.out.println("end test advice");
+    }
 
 
-  public static void main(String[] args) {
-    TimerPerson person = new TimerPerson();
-    person.httpClientTest();
-  }
+    public static void main(String[] args) {
+        TimerPerson person = new TimerPerson();
+        person.httpClientTest();
+    }
 }
